0e04483404edfc80e9fe6438d2144b022a579c67
santiago-restrepo <sanpra1208@gmail.com>
2020-05-26 08:50:51 -0500

    2. Add routing and initial components

---
+++ src/components/NewProduct.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewProduct = () => {
+	return (
+		<h1>Desde NewProduct.js</h1>
+	);
+}
+ 
+export default NewProduct;
---
+++ src/components/Products.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Products = () => {
+	return (
+		<h1>Desde Products.js</h1>
+	);
+}
+ 
+export default Products;
