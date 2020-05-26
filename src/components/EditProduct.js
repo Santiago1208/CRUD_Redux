3368fdc60f3d0c91454d16bdec16f6eedacb5dfd
santiago-restrepo <sanpra1208@gmail.com>
2020-05-26 09:06:32 -0500

    4. Add form to edit product and products list

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const EditProduct = () => {
 	return (
-		<h1>Desde EditProduct.js</h1>
+		<div className="row justify-content-center">
+			<div className="col-md-8 ">
+				<div className="card">
+					<div className="card-body">
+						<h2 className="text-center mb-4 font-weight-bold">Edit Product</h2>
+						<form>
+							<div className="form-group">
+								<label>Product Name</label>
+								<input type="text" className="form-control" placeholder="Product Name" name="name"></input>
+							</div>
+							<div className="form-group">
+								<label>Product Price</label>
+								<input type="number" className="form-control" placeholder="Product Price" name="price"></input>
+							</div>
+							<button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Save Changes</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
  
