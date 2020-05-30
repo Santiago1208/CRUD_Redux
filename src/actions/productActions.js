5d3a86f9cb5ab58d1d37d3ef8187c8eaec3a3d69
santiago-restrepo <sanpra1208@gmail.com>
2020-05-30 16:43:35 -0500

    9. Add a new product to the state

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -6,7 +6,29 @@ import {
 
 // Create new products
 export function actionCreateNewProduct(product) {
-	return () => {
-		console.log(product)
+	return (dispatch) => {
+		dispatch(addNewProduct());
+		try {
+			dispatch(addProductSucceed(product));
+		} catch (error) {
+			dispatch(addProductFailed(product));
+		}
 	}
 }
+
+const addNewProduct = () => ({
+	type: ADD_PRODUCT,
+	// This attribute is always used to modify the state.
+	// In this case, this action won't modify the state.
+	payload: null
+})
+
+const addProductSucceed = product => ({
+	type: ADD_PRODUCT_SUCCESS,
+	// In this case, this action will modify the state.
+	payload: product
+})
+
+const addProductFailed = product => ({
+	type: ADD_PRODUCT_ERROR,
+})
